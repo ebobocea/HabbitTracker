4cfbe0928a415645ab9780881de7cb99743399f9
elisei.bobocea <elisey93@icloud.com>
2022-09-21 15:55:25 +0100

    Completed groundwork

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
 import requests
+from datetime import datetime
+
+USERNAME = "elisei"
+TOKEN = "Srk3liseyD8gm73pta6"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_configuration = {
+    "id": "graph1",
+    "name": "Programming Practice",
+    "unit": "hr",
+    "type": "int",
+    "color": "momiji",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_configuration, headers=headers)
+# print(response.text)
+today = datetime.now()
+
+pixel_update_endpoint = f"{graph_endpoint}/{graph_configuration['id']}/{today.strftime('%Y%m%d')}"
+
+pixel_configuration = {
+    "quantity": "6"
+}
+
+response = requests.put(url=pixel_update_endpoint, headers=headers, json=pixel_configuration)
+print(response.text)
