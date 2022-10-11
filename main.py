8ef047bbc3d34977366eca8ea4710b83accc7856
Elisei Bobocea <39402539+ebobocea@users.noreply.github.com>
2022-10-11 15:10:56 +0000

    final version

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import os
 import requests
 from datetime import datetime
 
-print("working")
 USERNAME = os.environ['username']
 
-TOKEN =  os.environ['token']
+TOKEN = os.environ['token']
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
+  "token": TOKEN,
+  "username": USERNAME,
+  "agreeTermsOfService": "yes",
+  "notMinor": "yes",
 }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
@@ -22,16 +21,14 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_configuration = {
-    "id": "graph1",
-    "name": "Programming Practice",
-    "unit": "hr",
-    "type": "int",
-    "color": "momiji",
+  "id": "graph1",
+  "name": "Programming Practice",
+  "unit": "hr",
+  "type": "int",
+  "color": "momiji",
 }
 
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
+headers = {"X-USER-TOKEN": TOKEN}
 
 # response = requests.post(url=graph_endpoint, json=graph_configuration, headers=headers)
 # print(response.text)
@@ -39,9 +36,9 @@ today = datetime.now()
 
 pixel_update_endpoint = f"{graph_endpoint}/{graph_configuration['id']}/{today.strftime('%Y%m%d')}"
 
-pixel_configuration = {
-    "quantity": "1"
-}
+pixel_configuration = {"quantity": "1"}
 
-response = requests.put(url=pixel_update_endpoint, headers=headers, json=pixel_configuration)
+response = requests.put(url=pixel_update_endpoint,
+                        headers=headers,
+                        json=pixel_configuration)
 print(response.text)
